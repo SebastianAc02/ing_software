6ed0bea2f9e2c92bf0e22c1a446e2d1eaf7a35c1
Patil-Kranti <patilkranti03@gmail.com>
2024-02-03 00:07:41 +0530

    login logout completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <Button size="lg">Click Me</Button>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>This Screen is for Authenticated Users only</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
