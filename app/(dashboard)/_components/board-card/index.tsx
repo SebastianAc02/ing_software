9565deb5751fbc7fdf8673c1745829edf23c84af
Patil-Kranti <patilkranti03@gmail.com>
2024-02-09 22:13:20 +0530

    Added the card actions for copy link , delete card feature completed

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -7,6 +7,8 @@ import { useAuth } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
 import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Actions } from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
 
 interface BoardCardProps {
   id: string;
@@ -39,8 +41,21 @@ export const BoardCard = ({
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
         <div className="relative flex-1 bg-amber-50">
-          <Image src={imageUrl} fill className="object-fit" alt={title} />
+          <Image
+            src={imageUrl}
+            fill
+            className="object-fit"
+            alt={title}
+          />
           <Overlay />
+          <Actions
+            id={id}
+            title={title}
+            side="right">
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white transition-opacity opacity-75 hover:opacity-100" />
+            </button>
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
@@ -58,7 +73,7 @@ export const BoardCard = ({
 BoardCard.Skeleton = function BoardCardSkeleton() {
   return (
     <div className="aspect-[100/127] rounded-lg justify-between overflow-hidden">
-      <Skeleton className="h-full w-full" />
+      <Skeleton className="w-full h-full" />
     </div>
   );
 };
